6db56b6b008cb2b3072f4e2187d40adf430e5da6
Wilfred <wilfred5777@gmail.com>
2021-11-25 18:17:18 +0800

    initial Commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{-- {{ __('Dashboard') }} --}}
+            Hi, <b>{{ Auth:user()->name }}</b>
+            <b style="float:right"> Total Users
+            <span>{{ count($users) }}</span>
+            </b>
         </h2>
     </x-slot>
 
     <div class="py-12">
         {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> --}}
-            
+
                 {{-- <x-jet-welcome /> --}}
-                
+
                 <div class="container">
                     <div class="row">
                         <table class="table">
