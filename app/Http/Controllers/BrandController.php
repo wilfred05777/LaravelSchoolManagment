07017b286412d4154eb324edf7c808b1cfaa5458
Wilfred <wilfred5777@gmail.com>
2022-03-24 07:25:20 +0800

    40. Image Insert & Resize With Intervention Package

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+// use App\Http\Controllers\Image;
+use Image;
 
 class BrandController extends Controller
 {
@@ -29,13 +31,19 @@ class BrandController extends Controller
         ]);
 
         $brand_image = $request->file('brand_image');
-        $name_gen = hexdec(uniqid());
-        $img_ext = strtolower($brand_image->getClientOriginalExtension());
-        // $img_ext = strtolower($brand_image->guessExtension());
-        $img_name = $name_gen.'.'.$img_ext;
-        $up_location = 'image/brand/';
-        $last_img = $up_location.$img_name;
-        $brand_image->move($up_location,$img_name);
+
+        // $name_gen = hexdec(uniqid());
+        // $img_ext = strtolower($brand_image->getClientOriginalExtension());
+        // // $img_ext = strtolower($brand_image->guessExtension());
+        // $img_name = $name_gen.'.'.$img_ext;
+        // $up_location = 'image/brand/';
+        // $last_img = $up_location.$img_name;
+        // $brand_image->move($up_location,$img_name);
+
+        $name_gen = hexdec(uniqid()).'.'.$brand_image->getClientOriginalExtension();
+        Image::make($brand_image)->resize(300,200)->save('image/brand/'.$name_gen);
+
+        $last_img = 'image/brand/'.$name_gen;
 
         Brand::insert([
             'brand_name'  => $request->brand_name,
